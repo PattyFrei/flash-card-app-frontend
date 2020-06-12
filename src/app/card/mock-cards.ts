3f8bdcd30dc7866e90d98b6db776c425a259eacb
PattyFrei <patty.github@pm.me>
2020-06-12 16:06:51 +0200

    create card interface, mock data for cards

---
+++ src/app/card/mock-cards.ts
@@ -0,0 +1,274 @@
+import { Card } from './card';
+
+export const CARDS: Card[] = [
+  {
+    id: '0f202de2',
+    question: 'Was ist 5*5?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '3fcdb691',
+    question: 'Was ist 3*3?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: 'af94bdbe',
+    question: 'Was ist 6*6?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '0b9fdb34',
+    question: 'Was ist 7*7?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '9c405c6b',
+    question: 'Was ist 8*8?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '1b371635',
+    question: 'Was ist 9*9?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '065b78a7',
+    question: 'Was ist 5*7?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '304fd3ed',
+    question: 'Was ist 5*12?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '9346f626',
+    question: 'Was ist 5*15?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+  {
+    id: '20826cbc',
+    question: 'Was ist 17*5?',
+    questionType: 'radio',
+    answers: [
+      {
+        id: '497a2402',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: '794a941e',
+        correctAnswer: true,
+        text: 'adfaf',
+      },
+      {
+        id: 'c9dd252b',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+      {
+        id: 'bd08e312',
+        correctAnswer: false,
+        text: 'adfaf',
+      },
+    ],
+  },
+];
