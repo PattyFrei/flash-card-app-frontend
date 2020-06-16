5cc440702a9b2b0dc95227dc4bf14322b7d97d38
PattyFrei <patty.github@pm.me>
2020-06-16 11:10:51 +0200

    implement basic routing and nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeckComponent } from './deck/deck.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'deck', component: DeckComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
