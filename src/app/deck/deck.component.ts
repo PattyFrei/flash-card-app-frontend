1a5f08e0303ff699e42c696edc4ef65d74eb4532
PattyFrei <patty.github@pm.me>
2020-06-17 23:08:36 +0200

    clean up deck component

--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -12,7 +12,6 @@ export class DeckComponent implements OnInit {
   selectedCard: Card;
   // cards: Card[];
   deck: Deck;
-  selectedDeck: string;
 
   constructor(private deckService: DeckService) {
     // init local variables with values
@@ -20,21 +19,12 @@ export class DeckComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getDeck();
-  } // hock method, fetch data here
-
-  // getCards(): void {
-  //   // subscribe() passes the emitted array to the callback,
-  //   // which sets the component's cards property
-  //   this.deckService.getCards().subscribe((cards) => (this.cards = cards));
-  //   // this.cards = this.deckService.getCards();
-  // }
-
-  getDeck(): void {
+    // subscribe() passes the emitted array to the callback,
+    // which sets the component's cards property
     this.deckService
       .getDeck(this.deck.id)
       .subscribe((deck) => (this.deck = deck));
-  }
+  } // hock method, fetch data here
 
   onSelect(card: Card): void {
     this.selectedCard = card;
