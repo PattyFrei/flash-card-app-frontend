f81b7e3f22e9a5a76421dca79dc20a3a3dc9c01a
PattyFrei <patty.github@pm.me>
2020-07-27 21:29:32 +0200

    delete for my-collections

--- src/app/editor/my-collections/my-collections.component.ts
+++ src/app/editor/my-collections/my-collections.component.ts
@@ -4,6 +4,7 @@ import { MatSort } from '@angular/material/sort';
 
 import { Deck } from '../../deck/deck';
 import { DeckService } from '../../services/deck.service';
+import { SnackBarService } from '../../services/snack-bar.service';
 
 @Component({
   selector: 'app-my-collections',
@@ -11,7 +12,14 @@ import { DeckService } from '../../services/deck.service';
   styleUrls: ['./my-collections.component.scss'],
 })
 export class MyCollectionsComponent implements OnInit, AfterViewInit {
-  columnsToDisplay = ['name', 'topic', 'subject', 'questions', 'creationDate'];
+  columnsToDisplay = [
+    'name',
+    'topic',
+    'subject',
+    'questions',
+    'creationDate',
+    'delete',
+  ];
   decks = new MatTableDataSource<Deck>();
   isLoading = false;
   selectedId: string;
@@ -22,7 +30,10 @@ export class MyCollectionsComponent implements OnInit, AfterViewInit {
     return this.decks.data !== undefined;
   }
 
-  constructor(private deckService: DeckService) {}
+  constructor(
+    private deckService: DeckService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -39,6 +50,22 @@ export class MyCollectionsComponent implements OnInit, AfterViewInit {
 
   doFilter(value: string): void {
     this.decks.filter = value.trim().toLocaleLowerCase();
+  }
+
+  // TODO: confirm user selection with modal
+  // TODO: error case
+  deleteCollection(id: string, index: number): void {
+    this.deckService.deleteDeck(id).subscribe((data) => {
+      const successMessage = 'Das Quiz wurde erfolgreich gelöscht!';
+      this.removeCollectionFromTable(index);
+      this.snackBarService.open(successMessage);
+    });
+  }
+
+  removeCollectionFromTable(index: number): void {
+    // const index = this.cards.data.indexOf(item.id);
+    this.decks.data.splice(index, 1);
+    this.decks._updateChangeSubscription();
   }
 
   private dataLoaded(decks: Deck[]): void {
