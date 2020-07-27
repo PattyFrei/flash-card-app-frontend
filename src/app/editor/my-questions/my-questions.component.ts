5f8dbadadb6726d31333097b9e5ecc90e414d040
PattyFrei <patty.github@pm.me>
2020-07-27 20:55:38 +0200

    delete for my-questions

--- src/app/editor/my-questions/my-questions.component.ts
+++ src/app/editor/my-questions/my-questions.component.ts
@@ -4,6 +4,7 @@ import { MatSort } from '@angular/material/sort';
 
 import { Card } from '../../card/card';
 import { DeckService } from '../../services/deck.service';
+import { SnackBarService } from '../../services/snack-bar.service';
 
 @Component({
   selector: 'app-my-questions',
@@ -13,7 +14,7 @@ import { DeckService } from '../../services/deck.service';
 export class MyQuestionsComponent implements OnInit, AfterViewInit {
   cards = new MatTableDataSource<Card>();
   // columnsToDisplay = ['name', 'topic', 'subject', 'creationDate'];
-  columnsToDisplay = ['questionText', 'creationDate'];
+  columnsToDisplay = ['questionText', 'creationDate', 'delete'];
   isLoading = false;
   selectedId: string;
 
@@ -23,7 +24,10 @@ export class MyQuestionsComponent implements OnInit, AfterViewInit {
     return this.cards.data !== undefined;
   }
 
-  constructor(private deckService: DeckService) {}
+  constructor(
+    private deckService: DeckService,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -40,6 +44,22 @@ export class MyQuestionsComponent implements OnInit, AfterViewInit {
 
   doFilter(value: string): void {
     this.cards.filter = value.trim().toLocaleLowerCase();
+  }
+
+  // TODO: confirm user selection with modal
+  // TODO: error case
+  deleteQuestion(id: string, index: number): void {
+    this.deckService.deleteCard(id).subscribe((data) => {
+      const successMessage = 'Die Frage wurde erfolgreich gelöscht!';
+      this.removeQuestionFromTable(index);
+      this.snackBarService.open(successMessage);
+    });
+  }
+
+  removeQuestionFromTable(index: number): void {
+    // const index = this.cards.data.indexOf(item.id);
+    this.cards.data.splice(index, 1);
+    this.cards._updateChangeSubscription();
   }
 
   private dataLoaded(cards: Card[]): void {
