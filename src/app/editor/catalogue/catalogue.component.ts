bc98bdeb1318d5f80bf80945e08e918b1c6301e2
PattyFrei <patty.github@pm.me>
2020-08-04 13:38:11 +0200

    feat: edit existing catalogue

--- src/app/editor/catalogue/catalogue.component.ts
+++ src/app/editor/catalogue/catalogue.component.ts
@@ -1,10 +1,11 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
-import { Card } from '../../quiz/card/card';
+import { Card, Subject } from '../../quiz/card/card';
 import { DeckService } from '../../services/deck.service';
-import { Deck } from '../../quiz/deck/deck';
+import { Deck, Difficulty } from '../../quiz/deck/deck';
 import { SnackBarService } from '../../services/snack-bar.service';
 
 @Component({
@@ -14,14 +15,17 @@ import { SnackBarService } from '../../services/snack-bar.service';
 })
 export class CatalogueComponent implements OnInit {
   cards: Card[];
+  deck: Deck;
+  deckIsUpdating = false;
   difficulties: any;
-  isLoading = false;
   numberOfDefaultQuestions = 2;
+  selectedDifficulty: Difficulty;
+  selectedSubject: Subject;
   subjects: any;
   submitted = false;
   submittedDeck: Deck;
 
-  collectionForm = new FormGroup({
+  catalogueForm = new FormGroup({
     name: new FormControl('', Validators.required),
     topic: new FormControl(''),
     subject: new FormControl('', Validators.required),
@@ -29,19 +33,15 @@ export class CatalogueComponent implements OnInit {
     course: new FormControl(''),
     semester: new FormControl(''),
     difficulty: new FormControl('', Validators.required),
-    allowSecondGuess: new FormControl(false, Validators.required),
+    allowSecondGuess: new FormControl('', Validators.required),
     description: new FormControl(''),
     shareUrl: new FormControl(''),
-    shareUrlActive: new FormControl(false, Validators.required),
-    publicVisibility: new FormControl(false, Validators.required),
+    shareUrlActive: new FormControl('', Validators.required),
+    publicVisibility: new FormControl('', Validators.required),
   });
 
-  get isDataLoaded(): boolean {
-    return this.cards !== undefined;
-  }
-
   get form() {
-    return this.collectionForm;
+    return this.catalogueForm;
   }
 
   get shareUrlActive() {
@@ -52,29 +52,60 @@ export class CatalogueComponent implements OnInit {
     return this.form.get('publicVisibility').value;
   }
 
+  get subject() {
+    return this.form.get('subject').value;
+  }
+
   get questions() {
     return this.form.get('questions') as FormArray;
   }
 
   constructor(
     private deckService: DeckService,
-    private snackBarService: SnackBarService
+    private snackBarService: SnackBarService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.isLoading = true;
     this.getDifficulties();
-    this.getMyCards();
     this.getSubjects();
-    this.initFormQuestions();
+    this.getMyCards();
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.deckIsUpdating = true;
+      this.getDeck(id);
+    } else {
+      this.initFormQuestions();
+      this.form.patchValue({
+        allowSecondGuess: false,
+        publicVisibility: false,
+        shareUrlActive: false,
+      });
+    }
   }
 
   addQuestion(): void {
-    this.questions.push(new FormControl('', Validators.required));
+    const questionsGroup = new FormGroup({
+      id: new FormControl('', Validators.required),
+      questionText: new FormControl(''),
+    });
+    this.questions.push(questionsGroup);
   }
 
-  selectQuestion(formIndex: number, questionId: string): void {
-    this.questions.at(formIndex).get('id').patchValue(questionId);
+  addExistingQuestion(card: Card): void {
+    const questionsGroup = new FormGroup({
+      id: new FormControl(card.id, Validators.required),
+      questionText: new FormControl(card.questionText),
+    });
+    this.questions.push(questionsGroup);
+    console.log(questionsGroup);
+  }
+
+  getDeck(id: string): void {
+    this.deckService.getDeck(id).subscribe((deck) => {
+      this.deck = deck;
+      this.initUpdateForm();
+    });
   }
 
   getDifficulties(): void {
@@ -84,7 +115,7 @@ export class CatalogueComponent implements OnInit {
   }
 
   getMyCards(): void {
-    this.deckService.getMyCards().subscribe((cards) => this.dataLoaded(cards));
+    this.deckService.getMyCards().subscribe((cards) => (this.cards = cards));
   }
 
   getSubjects(): void {
@@ -102,7 +133,29 @@ export class CatalogueComponent implements OnInit {
     }
   }
 
-  onSubmit(collectionForm: FormGroup): void {
+  initUpdateForm(): void {
+    this.form.patchValue({
+      name: this.deck.name,
+      topic: this.deck.topic,
+      subject: this.deck.subject,
+      course: this.deck.course,
+      semester: this.deck.semester,
+      difficulty: this.deck.difficulty,
+      allowSecondGuess: this.deck.allowSecondGuess,
+      description: this.deck.description,
+      shareUrl: this.deck.shareUrl,
+      shareUrlActive: this.deck.shareUrlActive,
+      publicVisibility: this.deck.publicVisibility,
+    });
+
+    this.deck.questions.forEach((question) =>
+      this.addExistingQuestion(question)
+    );
+    this.selectedSubject = this.deck.subject;
+    this.selectedDifficulty = this.deck.difficulty;
+  }
+
+  onSubmit(catalogueForm: FormGroup): void {
     if (this.form.invalid) {
       const errorMessage = 'Die Angaben sind nicht vollständig.';
       this.snackBarService.open(errorMessage);
@@ -123,34 +176,43 @@ export class CatalogueComponent implements OnInit {
 
     const questions = [];
     for (let i = 0; i < this.questions.length; i++) {
-      questions.push(this.questions.at(i).value);
-      console.log(this.questions.at(i).value);
+      questions.push(this.questions.at(i).get('id').value);
+      console.log(this.questions.at(i).get('id').value);
     }
 
     this.submitted = true;
     const submittedDeck: Deck = {
-      name: collectionForm.value.name,
-      topic: collectionForm.value.topic,
-      subject: collectionForm.value.subject.name,
+      name: catalogueForm.value.name,
+      topic: catalogueForm.value.topic,
+      subject: catalogueForm.value.subject,
       questions,
-      course: collectionForm.value.course,
-      semester: collectionForm.value.semester,
-      difficulty: collectionForm.value.difficulty.level,
-      allowSecondGuess: collectionForm.value.allowSecondGuess,
-      description: collectionForm.value.description,
-      shareUrl: collectionForm.value.shareUrl,
-      shareUrlActive: collectionForm.value.shareUrlActive,
-      publicVisibility: collectionForm.value.publicVisibility,
+      course: catalogueForm.value.course,
+      semester: catalogueForm.value.semester,
+      difficulty: catalogueForm.value.difficulty,
+      allowSecondGuess: catalogueForm.value.allowSecondGuess,
+      description: catalogueForm.value.description,
+      shareUrl: catalogueForm.value.shareUrl,
+      shareUrlActive: catalogueForm.value.shareUrlActive,
+      publicVisibility: catalogueForm.value.publicVisibility,
     };
 
     console.log(submittedDeck);
 
-    this.deckService.createDeck(submittedDeck).subscribe((data) => {
-      const successMessage = 'Das Quiz wurde erfolgreich erstellt.';
-      this.snackBarService.open(successMessage);
-      console.log(data);
-      this.resetForm();
-    });
+    if (this.deckIsUpdating) {
+      this.deckService
+        .updateDeck(this.deck.id, submittedDeck)
+        .subscribe((data) => {
+          const successMessage = 'Die Frage wurde erfolgreich aktualisiert!';
+          this.snackBarService.open(successMessage);
+          this.resetForm();
+        });
+    } else {
+      this.deckService.createDeck(submittedDeck).subscribe((data) => {
+        const successMessage = 'Das Quiz wurde erfolgreich erstellt.';
+        this.snackBarService.open(successMessage);
+        this.resetForm();
+      });
+    }
   }
 
   removeQuestion(index: number): void {
@@ -180,24 +242,9 @@ export class CatalogueComponent implements OnInit {
     console.log(isShareUrlActive);
   }
 
-  setPublicVisibility(event: MatSlideToggleChange): void {
-    const isPubliclyVisible = event.checked;
-    console.log(isPubliclyVisible);
-  }
-
-  setAllowSecondGuess(event: MatSlideToggleChange): void {
-    const isAllowingSecondGuess = event.checked;
-    console.log(isAllowingSecondGuess);
-  }
-
   sortSubjects(): void {
     this.subjects.sort((a, b) =>
       a.name.localeCompare(b.name, 'de', { ignorePunctuation: true })
     );
-  }
-
-  private dataLoaded(cards: Card[]): void {
-    this.isLoading = false;
-    this.cards = cards;
   }
 }
