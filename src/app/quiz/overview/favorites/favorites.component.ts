84ef7ed1befe2c53e0d3c6d671f76d3aebcb5a31
PattyFrei <patty.github@pm.me>
2020-09-09 18:47:09 +0200

    add logic and styles for favorites component

--- src/app/quiz/overview/favorites/favorites.component.ts
+++ src/app/quiz/overview/favorites/favorites.component.ts
@@ -10,9 +10,10 @@ import { DeckService } from '../../../services/deck.service';
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.scss'],
 })
-export class FavoritesComponent implements OnInit {
+export class FavoritesComponent implements OnInit, AfterViewInit {
   columnsToDisplay = ['name', 'topic', 'subject', 'author', 'creationDate'];
   favorites = new MatTableDataSource<Deck>();
+  isLoading = false;
 
   @ViewChild(MatSort) sort: MatSort;
 
@@ -22,5 +23,27 @@ export class FavoritesComponent implements OnInit {
 
   constructor(private deckService: DeckService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.isLoading = true;
+    this.getFavorites();
+  }
+
+  ngAfterViewInit(): void {
+    this.favorites.sort = this.sort;
+  }
+
+  getFavorites(): void {
+    this.deckService
+      .getMyFavorites()
+      .subscribe((favorites) => this.dataLoaded(favorites));
+  }
+
+  doFilter(value: string): void {
+    this.favorites.filter = value.trim().toLocaleLowerCase();
+  }
+
+  private dataLoaded(favorites: Deck[]): void {
+    this.isLoading = false;
+    this.favorites.data = favorites;
+  }
 }
