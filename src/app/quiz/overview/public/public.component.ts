a94fa197466f4b2ef95862d5b8217cd85c09ccd8
PattyFrei <patty.github@pm.me>
2020-10-19 09:47:26 +0200

    update public quiz

--- src/app/quiz/overview/public/public.component.ts
+++ src/app/quiz/overview/public/public.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 
 import { Deck } from '../../quiz/../deck/deck';
-import { DeckService } from '../../../services/deck.service';
 
 @Component({
   selector: 'app-public',
@@ -11,39 +10,25 @@ import { DeckService } from '../../../services/deck.service';
   styleUrls: ['./public.component.scss'],
 })
 export class PublicComponent implements OnInit, AfterViewInit {
-  columnsToDisplay = ['name', 'topic', 'subject', 'author', 'creationDate'];
-  decks = new MatTableDataSource<Deck>();
+  columnsToDisplay = ['name', 'topic', 'subject', 'author'];
   isLoading = false;
 
+  @Input() decks: MatTableDataSource<Deck>;
   @ViewChild(MatSort) sort: MatSort;
 
   get isDataLoaded(): boolean {
     return this.decks.data.length >= 1;
   }
 
-  constructor(private deckService: DeckService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.isLoading = true;
-    this.getAllDecks();
-  }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     this.decks.sort = this.sort;
   }
 
-  getAllDecks(): void {
-    // subscribe() passes the emitted array to the callback,
-    // which sets the component's decks property
-    this.deckService.getDecks().subscribe((decks) => this.dataLoaded(decks));
-  }
-
   doFilter(value: string): void {
     this.decks.filter = value.trim().toLocaleLowerCase();
-  }
-
-  private dataLoaded(decks: Deck[]): void {
-    this.isLoading = false;
-    this.decks.data = decks;
   }
 }
